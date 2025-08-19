7df318eb145440632c8f9737211bf86cf8fe5f4a
Eltaf-azizi <iltfhuss@gmail.com>
2025-08-19 21:40:26 +0500

    working on the conditions

--- make_data.py
+++ make_data.py
@@ -44,3 +44,23 @@ def gen_password():
         "random16", "leetWord", "digitsOnly", "mix8to12"
     ]
 
+    p = random.choice(patterns)
+
+
+    if p == "word+year":
+        return rand_word() + str(random.randint(1990, 2025))
+    if p == "CapWord+digits":
+        return rand_word().capitalize() + rand_digits(random.randint(2, 4))
+    if p == "word123":
+        return rand_word() + "123"
+    if p == "Word!digits":
+        return rand_word().capitalize() + random.choice("!@#$") + rand_digits(random.randint(2, 4))
+    if p == "random16":
+        return rand_mix(16)
+    if p == "leetWord":
+        w = (rand_word()
+            .replace("a", "4").replace("e", "3")
+            .replace("i", "1").replace("o", "0").replace("s", "5"))
+        return w + random.choice(["!", "", "07"])
+
+
